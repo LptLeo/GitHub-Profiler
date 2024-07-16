6b9fb03e13ac58b6e52602c05b991d1538fdf63c
LptLeo <lptleo11@gmail.com>
2024-07-16 01:45:26 -0400

    Componentização de renderização de repositórios + barra de pesquisa por filtro

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { useQuery, gql } from '@apollo/client'
 
 import Search from './Components/Search'
 import Profile from './Components/Profile'
+import Repositories from './Components/Repositories'
 
 function App() {
   const [profile, setProfile] = useState("")
@@ -73,32 +74,7 @@ function App() {
           ) : (
             <Profile userData={data && data.user} />
           )}
-          <div className="row">
-            {data && data.user.repositories.nodes.map(repo => (
-              <div key={repo.id} className="repositorie">
-                <div className="repositorie-container">
-                  <a href={repo.url} target="_blank"><h5>{repo.name}</h5></a>
-                  <div className="h-100 pb-5">
-                    <div>
-                      <span className="lang-text">{repo.languages.edges.length !== 0 ? <b>Linguagens: </b> : <b>Nenhuma linguagem</b>}</span>
-                      {repo.languages.edges.map((lang, index) => (
-                        <span key={lang.node.id}>
-                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill={lang.node.color} className="bi bi-square-fill mb-1" viewBox="0 0 16 16">
-                            <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2z" />
-                          </svg>
-                          <> </>
-                          <span className="lang-values">{lang.node.name}{index < repo.languages.edges.length - 1 && ', '}</span>
-                        </span>
-                      ))}
-                    </div>
-                  </div>
-                  <div>
-                    <span className="repositorie-date"><b>Data: </b> {repo.createdAt.substring(0, 10).replaceAll("-", " / ")} </span>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
+          <Repositories userData={data && data.user} />
         </div>
       </div>
     </div>
